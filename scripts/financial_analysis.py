fda31322787dc6f6f39c1c0f06615a1c9a5af644
Ofgeha Gelana <ofgehagelana2019@gmail.com>
2024-12-13 14:11:43 +0300

    added top three publisher

--- scripts/financial_analysis.py
+++ scripts/financial_analysis.py
@@ -4,6 +4,24 @@ import numpy as np
 from textblob import TextBlob
 # import talib as tl
 
+
+def load_historical_data(ticker):
+    stock_data=pd.read_csv(f'../docs/yfinance_data/{ticker}_historical_data.csv')
+    return stock_data
+
+
+
 def get_sentiment(text):
     analysis = TextBlob(text)
     return analysis.sentiment.polarity
+
+
+def getSentimentAnalysisOfPublisher(news_data, target_publisher):
+    colors = {'positive': 'green', 'negative': 'red', 'neutral': 'blue'}
+    # Filter data for the target publisher
+    publisher_data = news_data[news_data['publisher'] == target_publisher]
+    sentiment_counts = publisher_data['sentiment_score_word'].value_counts().sort_index()
+
+    sentiment_counts.plot(kind="bar", figsize=(10, 4), title=f'Sentiment Analysis of {target_publisher}',
+                      xlabel='Sentiment categories', ylabel='Number of Published Articles',
+                      color=[colors[category] for category in sentiment_counts.index])
