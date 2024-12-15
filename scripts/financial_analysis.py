218a9d47532f2dbb0215584851da834fe16becd0
Ofgeha Gelana <ofgehagelana2019@gmail.com>
2024-12-15 10:39:13 +0300

    added portfolio analysis

--- scripts/financial_analysis.py
+++ scripts/financial_analysis.py
@@ -220,3 +220,37 @@ def closingPriceRelativeStrengthIndex(stock_data_aapl,stock_data_amzn,stock_data
     # momentum oscillator that measures the speed and change of price movements.
     # Identifying overbought and oversold conditions. A reading above 70 is typically 
     # considered overbought, while a reading below 30 is considered oversold.
+    
+    
+def calculatePortfolioWeightAndPerformance():
+    from pypfopt.efficient_frontier import EfficientFrontier
+    from pypfopt import risk_models
+    from pypfopt import expected_returns
+
+    tickers =['AAPL','AMZN','GOOG','MSFT','NVDA','META','TSLA']
+    # Load data from each ticker file
+    dataframes = [loadHistoricalData(ticker) for ticker in tickers]
+
+    # Combine dataframes into a single DataFrame
+    combined_data = pd.concat(dataframes, axis=1)['Close']
+
+    new_column_names = ['AAPL', 'AMZN','GOOG', 'META','MSFT','NVDA','TSLA'] 
+    combined_data.columns = new_column_names
+
+    # Calculate expected returns and sample covariance
+    mu = expected_returns.mean_historical_return(combined_data)
+    S = risk_models.sample_cov(combined_data)
+
+    # Optimize for maximal Sharpe ratio
+    ef = EfficientFrontier(mu, S)
+    weights = ef.max_sharpe()
+    weights = dict(zip(['AAPL', 'AMZN','GOOG', 'META','MSFT','NVDA','TSLA'],[round(value, 2) for value in weights.values()]))
+
+    # Print Portfolio weights
+    print("Portfolio Weights:")
+    print(weights)
+
+
+    # Calculate and print portfolio performance
+    print("\nPortfolio Performance:")
+    ef.portfolio_performance(verbose=True)
